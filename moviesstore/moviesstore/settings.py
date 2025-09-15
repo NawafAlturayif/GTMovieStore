d6bf2bbd97b39e73a2a8f885a48ba92d006874c8
Nawaf Alturayif <Nalturayif3@gatech.edu>
2025-09-15 11:29:46 -0400

    settings update

--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@kcg$7+-5uo)h!r(^m-q6eq#79wb3irk+=kw!qqfhwjhild+k4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home', 
+    'home',
     'movies',
     'accounts',
     'cart',
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
