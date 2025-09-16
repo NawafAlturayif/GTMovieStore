a8ac7683969589be4f30f83f50beb0ccb55ca9ea
Nawaf Alturayif <Nalturayif3@gatech.edu>
2025-09-16 14:24:07 -0400

    subscriptions feature

--- moviesstore/accounts/views.py
+++ moviesstore/accounts/views.py
@@ -6,6 +6,8 @@ from .forms import CustomUserCreationForm, CustomErrorList
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from cart.models import Order, Item # Import Order and Item models
+from django.db.models import Sum # Import Sum for aggregation
 
 
 @login_required
@@ -55,4 +57,36 @@ def orders(request):
     template_data['orders'] = request.user.order_set.all()
     return render(request, 'accounts/orders.html',
         {'template_data': template_data})
+
+@login_required
+def subscription_level(request):
+    template_data = {}
+    template_data['title'] = 'Subscription Level'
+
+    user = request.user
+    total_spent = 0
+
+    # Calculate total spending
+    # Get all orders for the current user
+    user_orders = Order.objects.filter(user=user)
+
+    # Sum the total price of all items in all orders
+    # This assumes Item has a 'price' and 'quantity' field
+    # and that 'price' is the price at the time of purchase.
+    for order in user_orders:
+        for item in order.item_set.all():
+            total_spent += item.price * item.quantity
+
+    # Determine subscription tier
+    if total_spent < 15:
+        subscription_tier = "Basic"
+    elif 15 <= total_spent < 30:
+        subscription_tier = "Medium"
+    else:
+        subscription_tier = "Premium"
+
+    template_data['total_spent'] = total_spent
+    template_data['subscription_tier'] = subscription_tier
+
+    return render(request, 'accounts/subscription_level.html', {'template_data': template_data})
 
